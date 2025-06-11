6f8c6c425eaa811324e3a512024faee4bd54a3f1
Simon Wang <wang.c.simon@gmail.com>
2025-06-11 18:19:47 -0400

    check dependencies

--- app/dashboard.py
+++ app/dashboard.py
@@ -9,6 +9,9 @@ import subprocess
 import sys
 import mlflow
 
+
+st.text(subprocess.getoutput("pip list"))
+
 # Dynamically add project root to path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if project_root not in sys.path:
